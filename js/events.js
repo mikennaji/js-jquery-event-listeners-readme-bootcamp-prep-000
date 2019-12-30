7397f4b3335a602094592f34879faed11e8e681d
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-30 12:49:46 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -22,6 +22,6 @@ function pressIt(){
 
 function submitIt(){
   $("form").on("submit", function(){
-    window.alert("your form is going to be submitted now");
+    window.alert("Your form is going to be submitted now");
   });
 }
