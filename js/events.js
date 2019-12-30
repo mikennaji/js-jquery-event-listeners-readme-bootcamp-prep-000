03c44b4e644520b19c251317a414e1440c2969f9
Michael Nnaji <mnnaji3@uwo.ca>
2019-12-30 12:54:05 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,29 @@
-//define functions here
+function getIt(){
+  $('p').on('click',function (){
+    window.alert("Hey!")
+  })
+}
 
-$(document).ready(function(){
+function frameIt(){
+  $('img').on('load', function(){
+  //actions you want to happen
+  $('img').addClass('tasty')
+})
+}
 
-// call functions here
+function pressIt(){
+  $('#typing').on('keydown', function(key) {
 
+  if(key.which == 71){
+      window.alert('Your form is going to be submitted now.');
+  }
 });
+}
+
+function submitIt(){
+  $('form').on('submit', function() {
+    window.alert('Your form is going to be submitted now.');
+    return;
+
+  });
+}
